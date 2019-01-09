18bc24784b3ed7afd105b6cab7d0da2f20c76cbf
Mark-Sandifer <msandifer96@gmail.com>
2019-01-09 17:15:11 +0000

    End of Wednesday

--- src/main/scala/CreditCard.scala
+++ src/main/scala/CreditCard.scala
@@ -1,5 +1,14 @@
 object CreditCard extends App {
-  def algorithm(input: Int): Unit ={
-    var
+  def algorithm(input: Long): List[Long] = {
+    val digits = (input.toString).toList.map(_.toString).map(_.toLong)
+    val checkDigit: Long = digits.last
+
+    val search: List[Long] =
+
+    println(digits)
+
+    digits
   }
+  algorithm(5204408086566492L)
 }
+
