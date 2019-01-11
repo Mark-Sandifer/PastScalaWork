c0d7923d0097bab141f88abade1db3594ecb33c1
Mark-Sandifer <msandifer96@gmail.com>
2019-01-11 15:32:06 +0000

    Thursday

--- src/main/scala/MorseCode.scala
+++ src/main/scala/MorseCode.scala
@@ -1,11 +1,7 @@
 import org.graalvm.compiler.graph.Node.Input
 
 object MorseCode extends App {
-//  def morseCode(input: String): Unit ={
-//    input match {
-//      case " ._ " =>
-//    }
-//  }
+  val morseArray = Array(".-")
   def splitSent(input: String): Unit ={
   println(input)
   val retVal = input.split(" / ")
@@ -13,15 +9,8 @@ object MorseCode extends App {
     splitWord(retVal)
   }
   def splitWord(input: Array[String]): Unit ={
-    val retVal2 = input.map(input => input.split(" "))
-    retVal2.foreach(println)
-    if(retVal2 == ".-"){
-      println("A")
-    }
+    val retVal2 = input.map(input => input.split(" ").foreach(println))
   }
 
-//  def formSentence(input: String): Unit ={
-//
-//  }
   splitSent(".- --.- .-. -..- - / .- --. -.-")
 }
